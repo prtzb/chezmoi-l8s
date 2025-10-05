773f60cbb12e770d73c3580fbb7e118f6b4fa7e9
Staffan Linnaeus <staffan.linnaeus@gmail.com>
2025-10-05 12:35:41 +0200

    Logrotate and longhorn config

--- root/scripts/run_once_after_01-setup-node.sh
+++ root/scripts/run_once_after_01-setup-node.sh
@@ -64,6 +64,7 @@ tee /etc/sysctl.d/k3s.conf <<EOF
 net.bridge.bridge-nf-call-iptables = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward = 1
+net.ipv4.ip_nonlocal_bind = 1
 EOF
 
 sysctl --system
